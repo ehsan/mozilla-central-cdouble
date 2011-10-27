840740d96eb761117824f02eb257a6421cb62011
Ehsan Akhgari <ehsan.akhgari@gmail.com>
2011-10-27 17:16:59 -0400

    Attempt to elevate privileges when updating in the background or replacing the application too

--- toolkit/mozapps/update/updater/updater.cpp
+++ toolkit/mozapps/update/updater/updater.cpp
@@ -2249,7 +2249,7 @@ int NS_main(int argc, NS_tchar **argv)
   // when write access is denied to the installation directory.
   HANDLE updateLockFileHandle = INVALID_HANDLE_VALUE;
   NS_tchar elevatedLockFilePath[MAXPATHLEN] = {NS_T('\0')};
-  if (argc > callbackIndex) {
+  if (argc > callbackIndex || sBackgroundUpdate || sReplaceRequest) {
     NS_tchar updateLockFilePath[MAXPATHLEN];
     NS_tsnprintf(updateLockFilePath,
                  sizeof(updateLockFilePath)/sizeof(updateLockFilePath[0]),
