75b1bec0a5b7ede48e93aba6a99235661e1c03dc
Ehsan Akhgari <ehsan.akhgari@gmail.com>
2011-11-30 17:48:09 -0500

    Fix the test to actually make it work

--- toolkit/mozapps/update/test/unit/test_0000_bootstrap_svc.js
+++ toolkit/mozapps/update/test/unit/test_0000_bootstrap_svc.js
@@ -24,15 +24,16 @@ function run_test() {
   do_test_pending();
   do_register_cleanup(cleanupUpdaterTest);
 
-  setupUpdaterTest(MAR_BOOTSTRAP_FILE);
+  setupUpdaterTest(MAR_COMPLETE_FILE);
 
   let updatesDir = do_get_file(TEST_ID + UPDATES_DIR_SUFFIX);
   let applyToDir = getApplyDirFile();
 
   // apply the complete mar
-  runUpdateUsingService(STATE_PENDING_SVC, STATE_SUCCEEDED, checkUpdateApplied);
+  runUpdateUsingService(STATE_PENDING_SVC, STATE_SUCCEEDED, checkUpdateApplied, null, false);
 }
 
 function checkUpdateApplied() {
   checkFilesAfterUpdateSuccess();
+  do_test_finished();
 }
