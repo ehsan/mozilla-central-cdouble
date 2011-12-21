958a06e3e5d11b8a9b3b8772f0fba7fac34b244d
Ehsan Akhgari <ehsan.akhgari@gmail.com>
2011-12-21 16:28:18 -0500

    Import Brian's work from 052e42822eeaa356641da070d3ddd45561f5e947
    http://hg.mozilla.org/projects/elm/052e42822eeaa356641da070d3ddd45561f5e947

--- toolkit/mozapps/update/common/updatehelper.cpp
+++ toolkit/mozapps/update/common/updatehelper.cpp
@@ -332,7 +332,7 @@ LaunchServiceSoftwareUpdateCommand(DWORD argc, LPCWSTR* argv)
   // it has 2 extra args: 1) The Path to udpater.exe, and 2) the command 
   // being executed which is "software-update"
   LPCWSTR *updaterServiceArgv = new LPCWSTR[argc + 2];
-  updaterServiceArgv[0] = L"MozillaMaintenance";
+  updaterServiceArgv[0] = L"maintenanceservice.exe";
   updaterServiceArgv[1] = L"software-update";
 
   for (int i = 0; i < argc; ++i) {
